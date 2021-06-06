bf87b9cba6092cce2e1cf51b531fadafe823c871
Nikita <65064358+lesnik34@users.noreply.github.com>
2021-06-06 11:06:11 +0300

    feat: changes

--- containers/ServicesContainer.js
+++ containers/ServicesContainer.js
@@ -1,4 +1,5 @@
 import { useSelector } from 'react-redux';
+import { useState } from 'react';
 import Image from 'next/image';
 
 import Services from '../components/Services/Services';
@@ -8,25 +9,46 @@ import styles from '../components/Services/Services.module.scss';
 const ServicesContainer = () => {
     const { services } = useSelector(state => state.content);
     const settings = useSelector(state => state.settings);
+    const [ modal, setModal ] = useState(null);
+    const [ vision, setVision ] = useState(false);
 
-    const getItems = () => services.images.map(image => (
-        <div key={image.id} className={styles.item}>
-            <div className={styles.imageWrap}>
-                <Image
-                    className={styles.image}
-                    src={getRightImage(image, settings).url}
-                    alt={image.imageName}
-                    width={getRightImage(image, settings).width}
-                    height={getRightImage(image, settings).height}
-                />
+    const openHandler = (image) => {
+        setModal(image);
+        setVision(true);
+        document.body.style.overflow = 'hidden';
+    };
+
+    const closeHandler = () => {
+        setVision(false);
+        document.body.style.overflow = 'auto';
+    };
+
+    const getItems = () => services.images.map(image => {
+        const currentImage = getRightImage(image, settings);
+
+        return (
+            <div key={image.id} className={styles.item}>
+                <div className={styles.imageWrap}>
+                    <Image
+                        className={styles.image}
+                        src={currentImage.url}
+                        alt={image.imageName}
+                        width={currentImage.width}
+                        height={currentImage.height}
+                        onClick={() => openHandler(currentImage)}
+                    />
+                </div>
             </div>
-        </div>
-    ));
+        );
+    });
 
     return <Services
         title={services.title}
         description={services.description.html}
         getItems={getItems}
+        modal={modal}
+        vision={vision}
+        closeHandler={closeHandler}
     />;
 };
 
