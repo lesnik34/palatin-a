261e70a89fbf64b8929d26593e5671c8aecad228
Nikita <65064358+lesnik34@users.noreply.github.com>
2021-09-06 18:43:15 +0300

    ref: tel refactor

--- containers/FooterContainer.js
+++ containers/FooterContainer.js
@@ -6,11 +6,25 @@ const FooterContainer = (props) => {
     const { inViewport, forwardedRef} = props;
     const { global } = useSelector(state => state.content);
 
+    const formatPhone = (phone) => `+${phone[0]} (${phone.slice(1, 4)}) ${phone.slice(4, 7)}-${phone.slice(7, 9)}-${phone.slice(9)}`;
+
+    const getPhones = () => (
+        <ul>
+            { global?.contactPhone?.map(el => (
+                <li key={el}>
+                    <a href={`tel:${el}`}>
+                        {formatPhone(el)}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    );
+
     return <Footer
         location={global.location ? global.location.html : ''}
         email={global.email}
         copyright={global.copyright}
-        phones={global.phoneNumbers ? global.phoneNumbers.html : ''}
+        phones={getPhones()}
         inViewport={inViewport}
         forwardedRef={forwardedRef}
     />;
